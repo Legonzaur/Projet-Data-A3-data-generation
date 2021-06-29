29aaa975bfdd4162a860e9f43a5e16130eb2c43f
Nathan TIEN YOU <nathan.tienyou@viacesi.fr>
2021-06-29 10:23:35 +0200

    graphe complet

--- genetic_custom/vrp_genetic_nathan.py
+++ genetic_custom/vrp_genetic_nathan.py
@@ -2,16 +2,22 @@ import sys
 import random
 import math
 
-# On a une grapue à 5 nodes et 8 arêtes
+# On a une graphe à 5 nodes et 8 arêtes
 
 vrp = [
-    [None, 3, None, None, 6],
-    [3, None, None, 2, 8]
-    [9, None, None, 1, 4],
-    [None, 2, 1, None, 5],
+    [None, 3, 9, 7, 6],
+    [3, None, 1, 2, 8]
+    [9, 1, None, 1, 4],
+    [7, 2, 1, None, 5],
     [6, 8, 4, 5, None]
 ]
 
 
-#
-# Premièrement, on lit le VRP
+def crossover(population, amount):
+    if(amount > len(population)):
+        raise Exception("crossover amount cannot exceed population amount")
+    output = []
+
+    for x in range(amount):
+        first = population[random.randint(0, len(population)-1)]
+    return
