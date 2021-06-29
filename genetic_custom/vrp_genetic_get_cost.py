90275774d53426a3963ba7176d207f98cd69fa6d
Nathan TIEN YOU <nathan.tienyou@viacesi.fr>
2021-06-29 15:55:02 +0200

    added get_cost

--- genetic_custom/vrp_genetic_get_cost.py
+++ genetic_custom/vrp_genetic_get_cost.py
@@ -1,2 +1,7 @@
-def get_gene_cost():
-    return 0
+def get_gene_cost(matrix, gene, initial=0):
+    current = initial
+    cost = 0
+    for i in gene:
+        cost += matrix[current][i]
+        current = i
+    return cost + matrix[current][initial]
--- vrp_test.py
+++ vrp_test.py
@@ -1,4 +1,7 @@
 # On a une grapue à 5 nodes et 8 arêtes
+from genetic_custom.vrp_genetic_get_cost import get_gene_cost
+
+
 vrp = [
     [None, 3, 9, 7, 6],
     [3, None, 1, 2, 8],
@@ -6,3 +9,5 @@ vrp = [
     [7, 2, 1, None, 5],
     [6, 8, 4, 5, None]
 ]
+
+print(get_gene_cost(vrp, [1, 2, 3, 4]))
