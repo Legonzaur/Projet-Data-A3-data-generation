4557456e5075256b5698985d8f2383519c18bdfc
Nathan TIEN YOU <nathan.tienyou@viacesi.fr>
2021-06-29 15:55:37 +0200

    cleaned pick best

--- genetic_custom/vrp_genetic_pick_best.py
+++ genetic_custom/vrp_genetic_pick_best.py
@@ -3,6 +3,3 @@ from genetic_custom.vrp_genetic_get_cost import get_gene_cost
 
 def get_best_chromosoms(population, amount):
     return sorted(population, key=get_gene_cost)[0:amount]
-
-
-print(get_best_chromosoms([[1, 2, 3, 4, 5, 6]], [1, 3, 5, 4, 6, 2], 1))
