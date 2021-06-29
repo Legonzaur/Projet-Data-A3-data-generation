08a6fe483aaa2c0e9bfd6101bd28b64dd14ec69e
Nathan TIEN YOU <nathan.tienyou@viacesi.fr>
2021-06-29 07:20:46 +0200

    Added graph

--- vrp_genetic_remi.py
+++ vrp_genetic_remi.py
@@ -2,7 +2,14 @@ import sys
 import random
 import math
 
+# On a une grapue à 5 nodes et 8 arêtes
 
-vrp = {}
+vrp = [
+    [None, 3, None, None, 6],
+    [3, None, None, 2, 8]
+    [9, None, None, 1, 4],
+    [None, 2, 1, None, 5],
+    [6, 8, 4, 5, None]
+]
 
-## Premièrement, on lit le VRP  
+# Premièrement, on lit le VRP
