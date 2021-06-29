2d1ab5192fd4c304cb4694d551c94ce264244b6b
Nathan TIEN YOU <nathan.tienyou@viacesi.fr>
2021-06-29 10:23:47 +0200

    Graphe complet

--- genetic_custom/vrp_genetic_remi.py
+++ genetic_custom/vrp_genetic_remi.py
@@ -5,10 +5,10 @@ import math
 # On a une grapue à 5 nodes et 8 arêtes
 
 vrp = [
-    [None, 3, None, None, 6],
-    [3, None, None, 2, 8]
-    [9, None, None, 1, 4],
-    [None, 2, 1, None, 5],
+    [None, 3, 9, 7, 6],
+    [3, None, 1, 2, 8]
+    [9, 1, None, 1, 4],
+    [7, 2, 1, None, 5],
     [6, 8, 4, 5, None]
 ]
 
