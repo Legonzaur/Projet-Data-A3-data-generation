310a9536c3c7fba84b2fd525d688fffa4977e268
Cnsbmkpsr <44788831+Cnsbmkpsr@users.noreply.github.com>
2021-06-29 12:13:37 +0200

    Update vrp_genetic_remi.py

--- genetic_custom/vrp_genetic_remi.py
+++ genetic_custom/vrp_genetic_remi.py
@@ -1,17 +1,15 @@
-import sys
 import random
-import math
-
-# On a une grapue à 5 nodes et 8 arêtes
 
 vrp = [
     [None, 3, 9, 7, 6],
-    [3, None, 1, 2, 8]
+    [3, None, 1, 2, 8],
     [9, 1, None, 1, 4],
     [7, 2, 1, None, 5],
     [6, 8, 4, 5, None]
 ]
 
-
-#
-# Premièrement, on lit le VRP
+def generate_gene(vrp):
+   a = list(range(len(vrp)))
+   random.shuffle(a)
+   return a
+print(generate_gene(vrp))
