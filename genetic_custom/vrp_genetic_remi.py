f2d6e2de5629902f8d436057a40aaaf3376ca402
Cnsbmkpsr <44788831+Cnsbmkpsr@users.noreply.github.com>
2021-06-30 15:15:56 +0200

    Update vrp_genetic_remi.py

--- genetic_custom/vrp_genetic_remi.py
+++ genetic_custom/vrp_genetic_remi.py
@@ -17,28 +17,28 @@ vehicles_stamped = db['vehicles_stamped']
 # Execution de la requete en utilisant python et datetime.datetime
 # Extraction des dates dinstinctes
 dates = list(\
-             vehicles_stamped.distinct("date")) #SOLUTION
+             vehicles_stamped.distinct("date"))
 # Initialisation de vehicules_par_minutes
 vehicules_par_minutes = dict(\
-                             [(date.strftime("%H:%M"), 0) for date in dates]) #SOLUTION
+                             [(date.strftime("%H:%M"), 0) for date in dates]) 
 
 # Parcours de la collection et calcul de la somme du trafic
 for trafic in vehicles_stamped.find():
-    date = trafic["date"].strftime("%H:%M") #SOLUTION
-    vehicules_par_minutes[date] += trafic["nb_vehicules"] #SOLUTION
+    date = trafic["date"].strftime("%H:%M") 
+    vehicules_par_minutes[date] += trafic["nb_vehicules"] 
 
 nb_jours = 5
 nb_data_points = nb_jours*len(vehicles_stamped.distinct("num_arete"))
 trafics = \
-    [vehicules_par_minutes[date]/nb_data_points for date in vehicules_par_minutes.keys()]#SOLUTION
+    [vehicules_par_minutes[date]/nb_data_points for date in vehicules_par_minutes.keys()]
 print("Traffic pour les 4 premières minutes :", trafics[0:4])
 
 # Les dates du matin
 xs = \
-    pd.date_range("2020-01-01 07:01", "2020-01-01 09:00", freq = "min").to_pydatetime().tolist() #SOLUTION
+    pd.date_range("2020-01-01 07:01", "2020-01-01 09:00", freq = "min").to_pydatetime().tolist() 
 # Le données de trafic au matin
 ys = \
-    trafics[:120] #SOLUTION
+    trafics[:120] 
 fig, axes = plt.subplots(2)
 # Traitements sur l'affichage au matin
 axes[0].xaxis.set_major_locator(mdates.HourLocator(interval = 1))
@@ -49,15 +49,15 @@ axes[0].plot(xs,ys,"o")
 
 # Les dates du soir
 xs = \
-    pd.date_range("2020-01-01 17:01", "2020-01-01 19:00", freq = "min").to_pydatetime().tolist() #SOLUTION# Les données de trafic au soir
+    pd.date_range("2020-01-01 17:01", "2020-01-01 19:00", freq = "min").to_pydatetime().tolist()# Les données de trafic au soir
 ys = \
-    trafics[120:] #SOLUTION
+    trafics[120:] 
 # Traitements sur l'affichage au soir
-axes[1].xaxis.set_major_locator(mdates.HourLocator(interval = 1)) #SOLUTION
-axes[1].xaxis.set_minor_locator(mdates.MinuteLocator(interval = 15)) #SOLUTION
-axes[1].xaxis.set_major_formatter(mdates.DateFormatter('%H:%M')) #SOLUTION
-axes[1].set(xlabel="temps", ylabel="trafic") #SOLUTION
-axes[1].plot(xs,ys,"o") #SOLUTION
+axes[1].xaxis.set_major_locator(mdates.HourLocator(interval = 1)) 
+axes[1].xaxis.set_minor_locator(mdates.MinuteLocator(interval = 15)) 
+axes[1].xaxis.set_major_formatter(mdates.DateFormatter('%H:%M')) 
+axes[1].set(xlabel="temps", ylabel="trafic") 
+axes[1].plot(xs,ys,"o") 
 
 fig.suptitle('Trafic moyen au matin et au soir.')
 plt.show()
@@ -65,11 +65,11 @@ plt.show()
 
 
 vehicules_par_arete = list(db.vehicles_stamped.aggregate([
-    {"$project":{"num_arete":1, "heures":{"$hour":"$date"}, "nb_vehicules":1}}, #SOLUTION
-    {"$match":{"heures":{"$lte":9, "$gte":7}}}, #SOLUTION
-    {"$group":{"_id":"$num_arete",  #SOLUTION
-               "nb_vehicules":{"$avg":"$nb_vehicules"}}}, #SOLUTION
-    {"$sort":{"nb_vehicules":-1}} #SOLUTION
+    {"$project":{"num_arete":1, "heures":{"$hour":"$date"}, "nb_vehicules":1}}, 
+    {"$match":{"heures":{"$lte":9, "$gte":7}}}, 
+    {"$group":{"_id":"$num_arete",  
+               "nb_vehicules":{"$avg":"$nb_vehicules"}}},
+    {"$sort":{"nb_vehicules":-1}}
 ]))
 print("Le résultat retourné par la requete (max,median,min):")
 print(vehicules_par_arete[0], vehicules_par_arete[249], vehicules_par_arete[-1])
