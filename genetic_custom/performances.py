f737c93acd5de98e7fd2c6522a628650d93ac498
Cnsbmkpsr <44788831+Cnsbmkpsr@users.noreply.github.com>
2021-07-07 17:21:21 +0200

    Stats qui marchent

--- genetic_custom/performances.py
+++ genetic_custom/performances.py
@@ -3,27 +3,29 @@ import matplotlib.pyplot as plt
 import json
 import matplotlib.pyplot as plt
 
+i = 0
 
-
-with open("DATA/stats_iterations (1).json", "r") as f:
+with open("DATA/stats_graph_size (1).json", "r") as f:
   data = json.load(f)
   execs = []
   sizes = []
   sizes2 = []
 for datapoint in data:
-  execs.append(float(datapoint["exec_time"]))
-  sizes.append(datapoint["iterations"])  
-  sizes2.append(datapoint["path_time"])  
+  if i < 70:
+    execs.append(float(datapoint["exec_time"]))
+    sizes.append(datapoint["iterations"])  
+    sizes2.append(datapoint["graph_size"])
+    i = i + 1
   
 # plotting the points 
 
 # plotting the points 
-plt.scatter(sizes,sizes2)
+plt.scatter(sizes2,execs)
   
 # naming the x axis
-plt.xlabel("Nombre d'itérations")
+plt.xlabel("Taille du graphe")
 # naming the y axis
-plt.ylabel("Longueur du meilleur chemin trouvé")
+plt.ylabel("Temps d'exécution(s)")
 
 # giving a title to my graph
 plt.title('Data')
